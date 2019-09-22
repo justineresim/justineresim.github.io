f625e4b632bce5987496d2b47ab0700d70324e6d
Justine Lileikis <justine@res.im>
2019-09-22 10:08:06 -0400

    Design updates

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -2,21 +2,4 @@ myApp.controller('HomeCtrl', function ($scope, $rootScope) {
 
     $rootScope.bodyClass = 'home';
 
-    $scope.companies = [
-        ["chrysler-white.png", "chrysler-black.png"],
-        ["asc-white.png", "asc-black.png"],
-        ["dodge-white.png", "dodge-black.png"],
-        ["res-white.png", "res.png"],
-        ["chrome-data-white.png", "chrome-data-black.png"],
-        ["infiniti-white.png", "infiniti.png"],
-        ["ram-white.png", "ram.png"],
-        ["fiat-white.png", "fiat.png"],
-        ["cc-white.png", "cc-black.png"],
-        ["jeep-white.png", "jeep.png"],
-        ["verto-white.png", "verto-black.png"],
-        ["gm-white.png", "gm-black.png"],
-        ["fhmatch-white.png", "fhmatch-black.png"],
-        ["inner-geek-white.png", "inner-geek-black.png"],
-    ];
-
 });
--- js/controllers/work.js
+++ js/controllers/work.js
@@ -1,4 +1,4 @@
-myApp.controller('WorkCtrl', function ($scope, $rootScope) {
+myApp.controller('WorkCtrl', function ($scope, $rootScope, $location) {
 
 	$rootScope.bodyClass = 'work';
 
@@ -6,6 +6,28 @@ myApp.controller('WorkCtrl', function ($scope, $rootScope) {
         var className = '.fancy-box-'+par;
         openFancyBox(className);
     }
+
+    $scope.updateTag = function(tag){
+        $scope.selectedTag = tag;
+    }
+
+
+    $scope.workFilter = function (item) { 
+        if($location.search().type == 'webapplications' || $location.search().type == 'websites' || $location.search().type == 'styleguides'){
+            $scope.selectedTag = $location.search().type;
+        }else{
+            return item; 
+        }
+        var found = false;
+        angular.forEach(item.tags, function(value, key) {
+            if($scope.selectedTag == value){
+                found = true;
+            }
+        });
+        if(found == true){
+            return item; 
+        }
+    };
 
 	$scope.work = [
         {
@@ -16,6 +38,7 @@ myApp.controller('WorkCtrl', function ($scope, $rootScope) {
             case: 'https://res.im/work/sait/',
             role: 'Front-end development',
             count: 17,
+            tags: ['websites', 'styleguides'],
             images: [
                 {
                     src: 'img/work/sait/1.png',
@@ -81,10 +104,11 @@ myApp.controller('WorkCtrl', function ($scope, $rootScope) {
         },
         {
             title: '3M Safety Admin Panel',
-            text: 'Find and edit product data, add products to your custom catelogues and create a professional PDF to share with clients.',
+            text: 'Find and edit product data, add products to your custom catalogues to create a professional PDF to share with clients.',
             image: 'img/work/admin/1.png',
             role: 'Development',
             count: 16,
+            tags: ['webapplications', 'styleguides'],
             images: [
                 {
                     src: 'img/work/admin/1.png',
@@ -114,6 +138,7 @@ myApp.controller('WorkCtrl', function ($scope, $rootScope) {
             image: 'img/work/safety/4.png',
             role: 'Development',
             count: 15,
+            tags: ['webapplications', 'styleguides'],
             images: [
                 {
                     src: 'img/work/safety/4.png',
@@ -151,6 +176,7 @@ myApp.controller('WorkCtrl', function ($scope, $rootScope) {
             image: 'img/work/jaws/1.png',
             role: 'Development',
             count: 14,
+            tags: ['websites'],
             images: [
                 {
                     src: 'img/work/jaws/1.png',
@@ -197,6 +223,7 @@ myApp.controller('WorkCtrl', function ($scope, $rootScope) {
             role: 'Development',
             url: 'https://res.im',
             count: 13,
+            tags: ['websites'],
             images: [
                 {
                     src: 'img/work/resim/1.png',
@@ -239,6 +266,7 @@ myApp.controller('WorkCtrl', function ($scope, $rootScope) {
             role: 'Development',
             url: 'https://precedentjd.com/',
             count: 12,
+            tags: ['websites'],
             images: [
                 {
                     src: 'img/work/student/1.png',
@@ -271,6 +299,7 @@ myApp.controller('WorkCtrl', function ($scope, $rootScope) {
             url: 'https://durhamcollege.ca/',
             // externalPrototype: 'http://review.durham.git.resolutionim.com/styleguide',
             count: 11,
+            tags: ['websites', 'styleguides'],
             images: [
                 {
                     src: 'img/work/durham/1.png',
@@ -307,401 +336,6 @@ myApp.controller('WorkCtrl', function ($scope, $rootScope) {
                 {
                     src: 'img/work/durham/9.png',
                     count: 11
-                }
-            ]
-        },
-        // {
-        //     title: 'VIQUA',
-        //     text: 'VIQUA is the world’s largest supplier of residential and light commercial UV water disinfection systems, providing safe water without the use of chemicals.',
-        //     image: 'img/work/viqua/viqua.png',
-        //     case: 'https://res.im/work/viqua/',
-        //     role: 'Development',
-        //     url: 'https://viqua.com/'
-        // },
-		{
-			title: 'ZTR Control Systems',
-            text: 'ZTR improves product design, warranty, legal, part sales, and dealer loyalty through telematics and data-driven decision making.',
-            image: 'img/work/ztr/ztr1.png',
-            prototype: 'ztr',
-            role: 'Front-end development',
-            count: 9,
-            images: [
-             	{
-                    src: 'img/work/ztr/ztr1.png',
-                    count: 9
-                },
-                {
-                    src: 'img/work/ztr/ztr2.png',
-                    count: 9
-                },
-                {
-                    src: 'img/work/ztr/ztr3.png',
-                    count: 9
-                },
-                {
-                    src: 'img/work/ztr/ztr4.png',
-                    count: 9
-                },
-                {
-                    src: 'img/work/ztr/ztr5.png',
-                    count: 9
-                },
-                {
-                    src: 'img/work/ztr/ztr6.png',
-                    count: 9
-                },
-                {
-                    src: 'img/work/ztr/ztr7.png',
-                    count: 9
-                },
-                {
-                    src: 'img/work/ztr/ztr8.png',
-                    count: 9
-                }
-            ]
-        },
-		// {
-		// 	title: 'Huron County',
-  //           text: 'Huron County is located along the shores of Lake Huron. This vibrant community is the most agriculturally productive county in Ontario.',
-  //           image: 'img/work/hc/hc3.png',
-  //           prototype: 'huron-county',
-  //           count: 8,
-  //           role: 'Front-end development',
-  //           images: [
-  //            	{
-  //                   desc: '<a target="_blank" href="work/huron-county/index.html" class="cta">View prototype <i class="fas fa-arrow-right"></i></a>',
-  //                   src: 'img/work/hc/hc3.png',
-  //                   count: 8
-  //               },
-  //               {
-  //                   desc: '<a target="_blank" href="work/huron-county/growing-together.html" class="cta">View prototype <i class="fas fa-arrow-right"></i></a>',
-  //                   src: 'img/work/hc/hc4.png',
-  //                   count: 8
-  //               },
-  //               {
-  //                   desc: '<a target="_blank" href="work/huron-county/childrens-services.html" class="cta">View prototype <i class="fas fa-arrow-right"></i></a>',
-  //                   src: 'img/work/hc/hc5.png',
-  //                   count: 8
-  //               },
-  //               {
-  //                   desc: '<a target="_blank" href="work/huron-county/events.html" class="cta">View prototype <i class="fas fa-arrow-right"></i></a>',
-  //                   src: 'img/work/hc/hc6.png',
-  //                   count: 8
-  //               },
-  //               {
-  //                   desc: '<a target="_blank" href="work/huron-county/index.html" class="cta">View prototype <i class="fas fa-arrow-right"></i></a>',
-  //                   src: 'img/work/hc/hc1.png',
-  //                   count: 8
-  //               },
-  //               {
-  //                   desc: '<a target="_blank" href="work/huron-county/events.html" class="cta">View prototype <i class="fas fa-arrow-right"></i></a>',
-  //                   src: 'img/work/hc/hc2.png',
-  //                   count: 8
-  //               },
-  //               {
-  //                   desc: '<a target="_blank" href="work/huron-county/global.html" class="cta">View prototype <i class="fas fa-arrow-right"></i></a>',
-  //                   src: 'img/work/hc/hc7.png',
-  //                   count: 8
-  //               },
-  //           	 {
-  //                   desc: '<a target="_blank" href="work/huron-county/global.html" class="cta">View prototype <i class="fas fa-arrow-right"></i></a>',
-  //                   src: 'img/work/hc/hc8.png',
-  //                   count: 8
-  //               },
-  //           ]
-  //       },
-		{
-			title: 'Confederation College',
-            text: 'Located in Northern Ontario, Confederation College offers practical, hands-on programs and courses across 9 campuses to over 7600 full and part-time students each year.',
-            image: 'img/work/confed/confed2.png',
-            prototype: 'confederation',
-            url: 'http://www.confederationcollege.ca/',
-            role: 'Front-end development',
-            count: 7,
-            images: [
-             	{
-                    src: 'img/work/confed/confed1.png',
-                    count: 7
-                },
-                {
-                    src: 'img/work/confed/confed2.png',
-                    count: 7
-                },
-				{
-                    src: 'img/work/confed/confed3.png',
-                    count: 7
-                },
-				{
-                    src: 'img/work/confed/confed4.png',
-                    count: 7
-                },
-				{
-                    src: 'img/work/confed/confed5.png',
-                    count: 7
-                }
-            ]
-        },
-		{
-			title: 'Golf Canada',
-            text: 'Canadian Golf Course History responsive website featuring an interactive map displaying Canadian golf courses throughout history.',
-            image: 'img/work/gc/gc1.png',
-            case: 'https://res.im/work/gc/gc/golf-canada/',
-            url: 'https://ch.golfcanada.ca/',
-            role: 'Development',
-            count: 6,
-            images: [
-             	{
-                    src: 'img/work/gc/gc1.png',
-                    count: 6
-                },
-                {
-                    src: 'img/work/gc/gc2.png',
-                    count: 6
-                },
-                
-                {
-                    src: 'img/work/gc/gc3.png',
-                    count: 6
-                },
-                {
-                    src: 'img/work/gc/gc4.png',
-                    count: 6
-                },
-                {
-                    src: 'img/work/gc/gc5.png',
-                    count: 6
-                },
-                {
-                    src: 'img/work/gc/gc6.png',
-                    count: 6
-                },
-                {
-                    src: 'img/work/gc/gc7.png',
-                    count: 6
-                }
-            ]
-        },
-	    // {
-     //        title: 'Verto360',
-     //        text: 'Verto360 is a company from London Ontario that designs, manages, builds and furnishes commercial building interiors.',
-     //        image: 'img/work/verto/verto1.png',
-     //        url: 'http://verto360.com/',
-     //        role: 'Development',
-     //        count: 3,
-     //        images: [
-     //            {
-     //                src: 'img/work/verto/verto1.png',
-     //                count: 3
-     //            },
-     //            {
-     //                src: 'img/work/verto/verto2.png',
-     //                count: 3
-     //            },
-     //            {
-     //                src: 'img/work/verto/verto3.png',
-     //                count: 3
-     //            },
-     //            {
-     //                src: 'img/work/verto/verto4.png',
-     //                count: 3
-     //            },
-     //            {
-     //                src: 'img/work/verto/verto5.png',
-     //                count: 3
-     //            },
-     //            {
-     //                src: 'img/work/verto/verto6.png',
-     //                count: 3
-     //            }
-     //        ]
-     //    },
-        // {
-        //     title: 'Fitness Health Match',
-        //     text: 'FHMatch is a social networking platform that connects people with a broad range of local fitness, health, and wellness professionals.',
-        //     image: 'img/work/fhm/fhm5.png',
-        //     url: 'http://www.fhmatch.com/',
-        //     role: 'Front-end development',
-        //     count: 1,
-        //     images: [
-        //         {
-        //             desc: '<a target="_blank" href="http://www.fhmatch.com/" class="cta">Visit website <i class="fas fa-arrow-right"></i></a>',
-        //             src: 'img/work/fhm/fhm1.png',
-        //             count: 1
-        //         },
-        //          {
-        //             desc: '<a target="_blank" href="http://www.fhmatch.com/register" class="cta">Visit website <i class="fas fa-arrow-right"></i></a>',
-        //             src: 'img/work/fhm/fhm2.png',
-        //             count: 1
-        //         },
-        //          {
-        //             desc: 'Browse fitness and health related articles.<br><a target="_blank" href="http://www.fhmatch.com/articles" class="cta">Visit website <i class="fas fa-arrow-right"></i></a>',
-        //             src: 'img/work/fhm/fhm3.png',
-        //             count: 1
-        //         },
-        //          {
-        //             desc: 'Easily find health and fitness professional in your area.<br><a target="_blank" href="http://www.fhmatch.com/" class="cta">Visit website <i class="fas fa-arrow-right"></i></a>',
-        //             src: 'img/work/fhm/fhm4.png',
-        //             count: 1
-        //         },
-        //          {
-        //             desc: 'Connect with professional to book appointments.<br><a target="_blank" href="http://www.fhmatch.com/" class="cta">Visit website <i class="fas fa-arrow-right"></i></a>',
-        //             src: 'img/work/fhm/fhm5.png',
-        //             count: 1
-        //         }
-        //     ]
-        // },
-       {
-            title: 'Redeemer University College',
-            text: 'Redeemer University College is an undergraduate university known for providing a high-quality liberal arts and sciences university education.',
-            image: 'img/work/redeemer/redeemer3.png',
-            url: 'https://www.redeemer.ca/',
-            case: 'https://res.im/work/redeemer/redeemer-university-college/',
-            role: 'Development',
-            count: 2,
-            images: [
-                {
-                    src: 'img/work/redeemer/redeemer1.png',
-                    count: 2
-                },
-                 {
-                    src: 'img/work/redeemer/redeemer2.png',
-                    count: 2
-                },
-                 {
-                    src: 'img/work/redeemer/redeemer3.png',
-                    count: 2
-                },
-                 {
-                    src: 'img/work/redeemer/redeemer4.png',
-                    count: 2
-                },
-                {
-                    src: 'img/work/redeemer/redeemer5.png',
-                    count: 2
-                },
-                {
-                    src: 'img/work/redeemer/redeemer6.png',
-                    count: 2
-                }
-            ]
-        },
-        {
-            title: 'Nissan Infiniti',
-            text: 'This prototype and style guide was created for Autodata Solutions as a starting point for the Nissan Infiniti Pricing Tool web application.',
-            image: 'img/work/ni/ni1.png',
-            prototype: 'nissan',
-            count: 10,
-            role: 'Front-end development',
-            images: [
-                {
-                    src: 'img/work/ni/ni1.png',
-                    count: 10
-                },
-                {
-                    src: 'img/work/ni/ni2.png',
-                    count: 10
-                },
-                {
-                    src: 'img/work/ni/ni3.png',
-                    count: 10
-                },
-                {
-                    src: 'img/work/ni/ni4.png',
-                    count: 10
-                },
-                {
-                    src: 'img/work/ni/ni5.png',
-                    count: 10
-                },
-                {
-                    src: 'img/work/ni/ni6.png',
-                    count: 10
-                }
-
-            ]
-        },
-        {
-            title: 'Build & Price',
-            text: 'Use Chrysler\'s Build and Price tool to customize your new vehicle exactly how you want it. Choose the color, trim, options and more and easily calculate the costs.',
-            image: 'img/work/bp/bp6.png',
-            role: 'Front-end development',
-            count: 4,
-            images: [
-                {
-                    src: 'img/work/bp/bp1.png',
-                    count: 4
-                },
-                {
-                    src: 'img/work/bp/bp3.png',
-                    count: 4
-                },
-                {
-                    src: 'img/work/bp/bp4.png',
-                    count: 4
-                },
-                {
-                    src: 'img/work/bp/bp5.png',
-                    count: 4
-                },
-                {
-                    src: 'img/work/bp/bp6.png',
-                    count: 4
-                },
-                {
-                    src: 'img/work/bp/bp7.png',
-                    count: 4
-                },
-                {
-                    src: 'img/work/bp/bp8.png',
-                    count: 4
-                },
-                {
-                    src: 'img/work/bp/bp9.png',
-                    count: 4
-                },
-                {
-                    src: 'img/work/bp/bp10.png',
-                    count: 4
-                },
-                {
-                    src: 'img/work/bp/bp11.png',
-                    count: 4
-                },
-                {
-                    src: 'img/work/bp/bp12.png',
-                    count: 4
-                },
-                {
-                    src: 'img/work/bp/bp13.png',
-                    count: 4
-                },
-                {
-                    src: 'img/work/bp/bp14.png',
-                    count: 4
-                },
-                {
-                    src: 'img/work/bp/bp15.png',
-                    count: 4
-                },
-                {
-                    src: 'img/work/bp/bp16.png',
-                    count: 4
-                },
-                {
-                    src: 'img/work/bp/bp17.png',
-                    count: 4
-                },
-                {
-                    src: 'img/work/bp/bp18.png',
-                    count: 4
-                },
-                {
-                    src: 'img/work/bp/bp19.png',
-                    count: 4
-                },
-                {
-                    src: 'img/work/bp/bp10.png',
-                    count: 4
                 }
             ]
         }
